7703305567715466a9750a67feb24fd8ecb3615f
xxxDSSxxx <dssmanta@gmail.com>
2019-10-01 13:33:47 +0300

    Update poker sorting.cpp

--- Algorithms/Sorting/poker sorting.cpp
+++ Algorithms/Sorting/poker sorting.cpp
@@ -1,7 +1,7 @@
 //create enum values
-
+enum Val { 2,3,4,5,6,7,8,9,10,J,Q,K,A };
 //create enum colors
-
+enum Color { clubs, diamonds, hearts, spades };
 //create array cards
 
 //create hand class
